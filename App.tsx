816e486c633e96e74bbf838104d3480351cb8699
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2024-01-02 15:23:07 +0530

    setup: React native cli and tailwindcss

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+
+function App(): React.JSX.Element {
+
+  return (
+    // <SafeAreaView style={backgroundStyle}>
+      <View className=" flex-1 bg-red-400"><Text>Clamp App</Text></View>
+    // </SafeAreaView>
+  );
+}
+
+export default App;
