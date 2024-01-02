c51112ee0ad05137081399dc30d7d16e9bb838c2
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2024-01-02 18:03:38 +0530

    feat: Navigation setup

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
+import 'react-native-gesture-handler';
+import {enableScreens} from 'react-native-screens';
+
 import React from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
-
+enableScreens();
 
 function App(): React.JSX.Element {
-
   return (
-    // <SafeAreaView style={backgroundStyle}>
-      <View className=" flex-1 bg-red-400"><Text>Clamp App</Text></View>
-    // </SafeAreaView>
+    <NavigationContainer>
+      <View className=" flex-1 bg-red-400">
+        <Text>Clamp App</Text>
+      </View>
+    </NavigationContainer>
   );
 }
 
